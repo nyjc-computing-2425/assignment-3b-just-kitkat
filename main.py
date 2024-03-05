b966b4796d1150b7e9f3d08aa75fa84a2dc84549
justkitkat <121209669+just-kitkat@users.noreply.github.com>
2024-03-05 12:02:41 +0000

    Submission

--- main.py
+++ main.py
@@ -2,3 +2,5 @@ stdform = input('Enter a number in scientific notation: ')
 stdform = stdform.strip()
 
 # Type your code below
+mantissa, exponent = stdform.split("x10^")
+print("This number in E notation is {}E{}.".format(mantissa, exponent))
